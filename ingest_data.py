b23176087faccd68e8a6745c90ce2886800d0f3a
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-03-04 18:39:29 -0800

    chat-my-fork

--- ingest_data.py
+++ ingest_data.py
@@ -1,23 +1,43 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader, DirectoryLoader
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
-
-# Load Data
-loader = UnstructuredFileLoader("state_of_the_union.txt")
-raw_documents = loader.load()
-
-# Split text
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(raw_documents)
+import os
 
 
-# Load Data to vectorstore
-embeddings = OpenAIEmbeddings()
-vectorstore = FAISS.from_documents(documents, embeddings)
+# Loader = UnstructuredFileLoader("state_of_the_union.txt")
+def embed_doc():
+    # check data folder is not empty
+    if len(os.listdir("data")) > 0:
+        loader = DirectoryLoader('data', glob="**/*.*")
+        raw_documents = loader.load()
+        print(len(raw_documents))
+        # Split text
+        text_splitter = RecursiveCharacterTextSplitter(
+            # Set a really small chunk size, just to show. 
+            chunk_size = 1000, 
+            chunk_overlap = 0, 
+            length_function = len,
+        )
+        print("111")
+        documents = text_splitter.split_documents(raw_documents)
+
+        # Load Data to vectorstore
+        embeddings = OpenAIEmbeddings()
+        print("222")
+        vectorstore = FAISS.from_documents(documents, embeddings)
+        print("333")
+
+        # Save vectorstore
+        # check if vectorstore.pkl exists
+        with open("vectorstore.pkl", "wb") as f:
+            pickle.dump(vectorstore, f)
 
 
-# Save vectorstore
-with open("vectorstore.pkl", "wb") as f:
-    pickle.dump(vectorstore, f)
+
+
+# check if vectorstore.pkl exists
+if os.path.exists("vectorstore.pkl"):
+    with open('vectorstore.pkl', 'rb') as f: 
+        docsearch = pickle.load(f)
